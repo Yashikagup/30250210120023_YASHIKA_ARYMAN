5ab8e666925ee807f5f785e817f0a7d9f260e934
gungungupta75@gmail.com <gungungupta75@gmail.com>
2026-03-31 22:10:11 +0530

    file

--- Classroom/Detect_loop_in_ll.java
+++ Classroom/Detect_loop_in_ll.java
@@ -1,4 +1,14 @@
+class ListNode {
+    int val;
+    ListNode next;
 
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
 public class Detect_loop_in_ll {
     public boolean hasCycle(ListNode head) { 
         if (head == null) return false;
--- Classroom/Tortoise_algo.java
+++
@@ -1,3 +0,0 @@
-public class Tortoise_algo {
-    
-}
